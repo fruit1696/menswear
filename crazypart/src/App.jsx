576f15db1bf5f0d43537c064df78d5d82c57835c
lichi-sys <lichi@theblock.me>
2026-09-06 19:55:41 -0400

    enable vercel analytics

--- crazypart/src/App.jsx
+++ crazypart/src/App.jsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
@@ -62,6 +63,7 @@ function App() {
                     <AuthenticatedApp />
                 </Router>
                 <Toaster />
+                <Analytics />
             </QueryClientProvider>
         </AuthProvider>
     )
